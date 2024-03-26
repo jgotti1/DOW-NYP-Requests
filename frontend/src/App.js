ed9247faf1e28c0e6b06d5c80c39d6f780e65a8d
Margotti <margottij@win.dowjones.net>
2024-03-26 15:41:08 -0400

    started modal for data entry

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
+import React, { useState } from "react";
 import "./App.css";
 import DataTable from "./components/DataTable";
 import FilterableList from "./components/FilterableList";
+import DataEntryModal from "./components/DataEntryModal"; // Import your modal component
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [modalData, setModalData] = useState({}); // Data to edit, if necessary
+
+  const handleOpenModal = (data = {}) => {
+    setModalData(data); // Set the data you might want to edit
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleSaveModalData = (data) => {
+    console.log("Save data", data);
+    // Here you would handle saving the data, possibly sending a PUT request
+    handleCloseModal();
+  };
+
   return (
     <div className="App">
       <div className="head-container">
@@ -13,6 +33,8 @@ function App() {
       </div>
       <div className="filter-list-div">
         <FilterableList />
+      <button onClick={() => handleOpenModal()}>Add/Edit Request</button> {/* Button to open the modal */}
+      <DataEntryModal show={showModal} handleClose={handleCloseModal} handleSubmit={handleSaveModalData} initialData={modalData} />
       </div>
       <DataTable />
     </div>
