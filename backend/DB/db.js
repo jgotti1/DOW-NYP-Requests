79c4e6d8b9e2263939db14e81d13057b9875f228
Margotti <margottij@win.dowjones.net>
2024-04-08 12:02:12 -0400

    added db connect change for AWS support

--- backend/DB/db.js
+++ backend/DB/db.js
@@ -1,4 +1,4 @@
-require("dotenv").config(); 
+require("dotenv").config();
 
 const { Pool } = require("pg");
 
@@ -7,7 +7,10 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
