7800d8e318c0c3072cc52f4c5c751a38f3515f6c
Margotti <margottij@win.dowjones.net>
2024-04-25 12:52:47 -0400

    aws deploy prod changes

--- backend/DB/db.js
+++ backend/DB/db.js
@@ -8,6 +8,7 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  // For AWS deploy below
   ssl: {
     rejectUnauthorized: false,
   },
